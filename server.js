32f25704f3708b3c6aefcf61c10720e998653f18
hodophile ram <86426602+nagasairam1@users.noreply.github.com>
2025-09-02 14:55:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-// Import routes
+// Serve frontend
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
 const authRoutes = require("./routes/auth");
 app.use("/auth", authRoutes);
 
 app.get("/", (req, res) => {
-  res.send("SRMsolutions Backend Running ✅");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
